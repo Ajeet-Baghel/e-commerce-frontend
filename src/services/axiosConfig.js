c5a7da00f047491cbd7c7a9caabf875da77b3d59
Ajeet Baghel <ajeetbaghelcink@gmail.com>
2025-10-14 16:13:21 +0530

    add api base url for backend

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: import.meta.env.DEV
+    ? "http://localhost:4000"
+    : "https://ajeet-node-ecommerce.netlify.app",
   headers: {
     "Content-Type": "application/json",
   },
